088fa970490af687045667a13c53406d843669e9
Atte Keinänen <atte.keinanen@gmail.com>
2014-09-02 11:59:03 +0300

    Renaming to avoid conflict with Menu class name

--- app/src/main/java/studies/drawingapp/MainMenu.java
+++ app/src/main/java/studies/drawingapp/MainMenu.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Menu extends Activity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main_menu);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
