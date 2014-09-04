4bb988676834165a2028fd82e3f757c6dafa8dc9
LaBSo <jooel.friman@gmail.com>
2014-09-04 14:52:02 +0300

    Lisätty piirtotyökaluja.

--- app/src/main/java/studies/drawingapp/DrawingCanvas.java
+++ app/src/main/java/studies/drawingapp/DrawingCanvas.java
@@ -4,13 +4,20 @@ import studies.drawingapp.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 
 /**
@@ -20,10 +27,59 @@ import android.widget.TextView;
  * @see SystemUiHider
  */
 public class DrawingCanvas extends Activity {
+    private Path drawPath;
+
+    private Paint drawPaint, canvasPaint;
+
+    private int paintColor = 0xFF660000;
+
+    private Canvas drawCanvas;
+
+    private Bitmap canvasBitmap;
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+
+    protected void setupDrawing (){
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    //detect user touch
+        float touchX = event.getX();
+        float touchY = event.getY();
+    }
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
+
     private static final boolean AUTO_HIDE = true;
 
     /**
@@ -48,6 +104,7 @@ public class DrawingCanvas extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
+    // Nimi ikkuna
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
