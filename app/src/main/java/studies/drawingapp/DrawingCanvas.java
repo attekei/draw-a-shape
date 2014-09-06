739af1d716ede3c3bac2c3a64a3ddca4fcb9cd99
Atte Keinänen <atte.keinanen@gmail.com>
2014-09-06 20:23:52 +0300

    Move drawing behavior to custom view DrawingCanvasView. Hide title bar.

--- app/src/main/java/studies/drawingapp/DrawingCanvas.java
+++ app/src/main/java/studies/drawingapp/DrawingCanvas.java
@@ -2,90 +2,35 @@ package studies.drawingapp;
 
 import studies.drawingapp.util.SystemUiHider;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ListView;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.view.MotionEvent;
 
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class DrawingCanvas extends Activity {
-    private Path drawPath;
-
-    private Paint drawPaint, canvasPaint;
-
-    private int paintColor = 0xFF660000;
-
-    private Canvas drawCanvas;
-
-    private Bitmap canvasBitmap;
-
-    public DrawingView(Context context, AttributeSet attrs){
-        super(context, attrs);
-        setupDrawing();
-    }
-
-    protected void setupDrawing (){
-        drawPath = new Path();
-        drawPaint = new Paint();
-
-        drawPaint.setColor(paintColor);
-        drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20);
-        drawPaint.setStyle(Paint.Style.STROKE);
-        drawPaint.setStrokeJoin(Paint.Join.ROUND);
-        drawPaint.setStrokeCap(Paint.Cap.ROUND);
-        canvasPaint = new Paint(Paint.DITHER_FLAG);
-
-    }
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        drawCanvas = new Canvas(canvasBitmap);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-    //detect user touch
-        float touchX = event.getX();
-        float touchY = event.getY();
-    }
-
-    // Nimi ikkuna
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        enableFullscreen();
+
         setContentView(R.layout.activity_drawing_canvas);
-
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final TextView contentView = (TextView) findViewById(R.id.fullscreen_content);
-
-        final String username = getIntent().getExtras().getString("username");
-        contentView.setText("Piirretään käyttäjälle: " + username);
     }
 
+    private void enableFullscreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 }
+
