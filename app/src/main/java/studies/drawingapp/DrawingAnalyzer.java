3371571e53a41a4da1440a086df529eb4676fdf0
Atte Keinänen <atte.keinanen@gmail.com>
2014-10-26 17:59:45 +0200

    Add bitmap scaling and logging to DrawingAnalyzer

--- app/src/main/java/studies/drawingapp/DrawingAnalyzer.java
+++ app/src/main/java/studies/drawingapp/DrawingAnalyzer.java
@@ -2,11 +2,14 @@ package studies.drawingapp;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 
 import studies.algorithms.CMAESGuess;
 import studies.algorithms.PointCloud;
 
 public class DrawingAnalyzer {
+    private static final String TAG = "DrawingAnalyzer";
+
     final int emptyColor = Color.WHITE;
     final int pointColor = Color.BLACK;
     final int samples = 100;
@@ -16,26 +19,48 @@ public class DrawingAnalyzer {
     private PointCloud resultCloud;
 
     public DrawingAnalyzer(Bitmap drawing, Bitmap model) {
+
         modelCloud = this.getCenteredCloud(model);
+
+        Log.i(TAG, "Model cloud aligned");
         PointCloud alignedDrawingCloud =
                 this.getCenteredCloud(drawing).alignByStandardDeviation(modelCloud);
+
+        Log.i(TAG, "Drawing cloud aligned");
 
         guess = alignedDrawingCloud.downsample(samples).runCMAES(
                 modelCloud.downsample(samples)
         );
 
+        Log.i(TAG, "Clouds downsampled");
+
         resultCloud = alignedDrawingCloud.transformByCMAESGuess(guess);
+
+        Log.i(TAG, "Transformation completed");
     }
 
     private PointCloud getCenteredCloud(Bitmap bitmap) {
-        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
-        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        Bitmap scaledBitmap = scaleToFill(bitmap, 1000, 1000);
 
-        PointCloud cloud = PointCloud.fromImagePixelArray(pixels, bitmap.getWidth(), pointColor);
+        int[] pixels = new int[scaledBitmap.getWidth() * scaledBitmap.getHeight()];
+        scaledBitmap.getPixels(pixels, 0, scaledBitmap.getWidth(), 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight());
+
+        Log.i(TAG, "Getting centered cloud: " + scaledBitmap.getWidth());
+
+        PointCloud cloud = PointCloud.fromImagePixelArray(pixels, scaledBitmap.getWidth(), pointColor);
+        Log.i(TAG, "We've got a cloud! It's width is " + cloud.width());
         PointCloud centeredCloud = cloud.centerByMean();
 
         return centeredCloud;
     }
+
+    private Bitmap scaleToFill(Bitmap b, int width, int height) {
+        float factorH = height / (float) b.getWidth();
+        float factorW = width / (float) b.getWidth();
+        float factorToUse = (factorH > factorW) ? factorW : factorH;
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factorToUse), (int) (b.getHeight() * factorToUse), false);
+    }
+
 
     public CMAESGuess getGuess() { return guess; }
 
